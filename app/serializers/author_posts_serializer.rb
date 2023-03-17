79e9e23b93be3e85c482b4f2a94859c846542085
ianK-code <kian99564@gmail.com>
2023-03-17 14:54:46 +0300

    2 test remaining

--- app/serializers/author_posts_serializer.rb
+++ app/serializers/author_posts_serializer.rb
@@ -1,9 +1,11 @@
 class AuthorPostsSerializer < ActiveModel::Serializer
   attributes :title, :short_content
+  belongs_to :author
+  # has_many :tags, serializer: TagSerializer
 
   def short_content
     if object.content.length > 40
-      object.content[0..39]
+      object.content[0..39] + '...'
     else
       object.content
     end
